d34e64272da181e909ac43c11aeff2c964454a54
Artur Oscar <Arturoscar3@gmail.com>
2022-09-28 23:01:14 -0300

    Adding initial settings and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.14.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "com.decoder.authuser"
@@ -18,8 +19,10 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/authuser/AuthuserServiceApplication.kt
+++ src/main/kotlin/authuser/AuthuserServiceApplication.kt
@@ -7,6 +7,5 @@ import org.springframework.boot.runApplication
 class AuthuserServiceApplication
 
 fun main(args: Array<String>) {
-
     runApplication<AuthuserServiceApplication>(*args)
 }
